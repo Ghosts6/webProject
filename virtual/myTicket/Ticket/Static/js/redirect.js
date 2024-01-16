38fbc388a077004141bb7030fd7e42d0dcd48c97
Kiarash <95994481+Ghosts6@users.noreply.github.com>
2024-01-16 09:03:13 +0330

    Update redirect.js

--- virtual/myTicket/Ticket/Static/js/redirect.js
+++ virtual/myTicket/Ticket/Static/js/redirect.js
@@ -1,4 +1,4 @@
-// Function to get current time in Tehran timezone
+
 function getCurrentTimeInTehran() {
   const currentTimeElement = document.getElementById('currentTime');
   if (currentTimeElement) {
@@ -7,32 +7,31 @@ function getCurrentTimeInTehran() {
   }
 }
 
-// Function to update user's name
+
 function updateUserName() {
   const userNameElement = document.querySelector('.user-info h2');
   if (userNameElement) {
-    // Replace 'John Doe' with the actual user's name
-    userNameElement.textContent = 'John Doe'; // Replace with the fetched user's name
-    // You can fetch the user's name from your backend or any other source here
+
+    userNameElement.textContent = 'John Doe'; 
+
   }
 }
 
-// Function to redirect to main.html
+
 function redirectToMain() {
-  window.location.href = "main.html"; // Redirect to main.html
+  window.location.href = "main.html"; 
 }
 
-// Function to redirect to status.html
+
 function redirectToStatus() {
-  window.location.href = "status.html"; // Redirect to status.html
+  window.location.href = "status.html"; 
 }
 
-// Event listener for when the DOM content is loaded
+
 document.addEventListener("DOMContentLoaded", function () {
-  // Call functions to get current time and update user's name
+ 
   getCurrentTimeInTehran();
   updateUserName();
 
-  // Update current time in Tehran timezone every second
   setInterval(getCurrentTimeInTehran, 1000);
 });
