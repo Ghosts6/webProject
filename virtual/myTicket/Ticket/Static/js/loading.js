472fa1ec5788b341a1abc045e8192c5a02f195e7
Kiarash <95994481+Ghosts6@users.noreply.github.com>
2024-01-16 09:01:18 +0330

    Update loading.js

--- virtual/myTicket/Ticket/Static/js/loading.js
+++ virtual/myTicket/Ticket/Static/js/loading.js
@@ -1,15 +1,15 @@
 function createTicket() {
-    // Replace this with your ticket creation logic or redirection to the ticket creation page
+
     alert('Redirect to ticket creation page!');
   }
 function redirectToMain() {
-  window.location.href = "main.html"; // Redirect to main.html
+  window.location.href = "main.html"; 
 } 
 function updateClock() {
   const clockElement = document.getElementById('clock');
   const today = new Date();
   
-  // Set timezone to Iran/Tehran
+
   const options = { timeZone: 'Asia/Tehran' };
   const currentTime = today.toLocaleTimeString('en-US', options);
   const currentDate = today.toLocaleDateString('en-US', { timeZone: 'Asia/Tehran' });
@@ -17,5 +17,5 @@ function updateClock() {
   clockElement.innerHTML = `Today's Date: ${currentDate}<br>Current Time: ${currentTime}`;
 }
 
-// Update clock every second
+
 setInterval(updateClock, 1000);
