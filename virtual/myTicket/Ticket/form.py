7d48603d7821bbf52cabbcfa8684ea18511df224
Kiarash <95994481+Ghosts6@users.noreply.github.com>
2024-01-16 22:12:11 +0000

    Update form.py

--- virtual/myTicket/Ticket/form.py
+++ virtual/myTicket/Ticket/form.py
@@ -5,3 +5,8 @@ class ITRequestForm(forms.ModelForm):
     class Meta:
         model = ITRequest
         fields = '__all__'
+
+class TicketForm(forms.ModelForm):
+    class Meta:
+        model = Ticket
+        fields = ['title', 'description', 'admin_note', 'status']
