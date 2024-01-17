a4e80519d6f387b0704fe5d91596746910a9cd61
Kiarash <95994481+Ghosts6@users.noreply.github.com>
2024-01-17 17:16:14 +0330

    Update models.py

--- virtual/myTicket/Ticket/models.py
+++ virtual/myTicket/Ticket/models.py
@@ -2,28 +2,20 @@ from django.contrib.auth.models import AbstractUser, User
 from django.db import models
 
 class CustomUser(AbstractUser):
-    # Add your custom fields here if needed
-    # For example:
-    # custom_field = models.CharField(max_length=100)
-
     class Meta:
-        # This will avoid table name clashes if your app_label is the same as auth
         db_table = 'custom_user'
         app_label = 'Ticket'
-        # You can also specify other Meta options as needed
 
-# Ensure to provide related_name for groups and user_permissions
 CustomUser._meta.get_field('groups').remote_field.related_name = 'custom_user_groups'
 CustomUser._meta.get_field('user_permissions').remote_field.related_name = 'custom_user_permissions'
 
 class YourModel(models.Model):
-    # Define fields matching the structure of your SQL data
     field1 = models.CharField(max_length=100)
     field2 = models.IntegerField()
-    # Add more fields as needed
+
 
     def __str__(self):
-        return self.field1  # Return a meaningful representation
+        return self.field1
         
 class Tickets(models.Model):
     TicketID = models.AutoField(primary_key=True)
@@ -38,13 +30,13 @@ class Tickets(models.Model):
 class TicketNotes(models.Model):
     note_id = models.AutoField(primary_key=True)
     ticket = models.ForeignKey(Tickets, on_delete=models.CASCADE)
-    admin = models.ForeignKey(CustomUser, on_delete=models.CASCADE)  # Using your CustomUser model here
+    admin = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     note_text = models.TextField()
     change_status = models.CharField(max_length=12, choices=[('done', 'Done'), ('in_progress', 'In Progress'), ('not_seen', 'Not Seen')])
     timestamp = models.DateTimeField(auto_now_add=True)
 
 class LoginAttempt(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)  # Using your CustomUser model here
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
     failed_attempts = models.PositiveIntegerField(default=0)
 
